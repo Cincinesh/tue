0bb01d22425a0d66d74c6eecc14da63c5d2e2f6e
Jo Bates <cincinesh@icloud.com>
2015-02-07 11:09:12 -0600

    Prepared vec.hpp for publication

--- include/tue/vec.hpp
+++ include/tue/vec.hpp
@@ -7,36 +7,57 @@
 
 namespace tue
 {
-	// ---------
-	// vec<T, N>
-	// ---------
-	template<typename T, int N>
-	class vec;
+  // An N-component vector with components of the given type.
+  template<typename T, int N>
+  class vec;
 
-	template<typename T>
-	using vec2 = vec<T, 2>;
+  // A 2-component vector with components of the given type.
+  template<typename T>
+  using vec2 = vec<T, 2>;
 
-	template<typename T>
-	using vec3 = vec<T, 3>;
+  // A 3-component vector with components of the given type.
+  template<typename T>
+  using vec3 = vec<T, 3>;
 
-	template<typename T>
-	using vec4 = vec<T, 4>;
+  // A 4-component vector with components of the given type.
+  template<typename T>
+  using vec4 = vec<T, 4>;
 
-	using fvec2 = vec2<float>;
-	using fvec3 = vec3<float>;
-	using fvec4 = vec4<float>;
+  // A 2-component vector of float's.
+  using fvec2 = vec2<float>;
 
-	using dvec2 = vec2<double>;
-	using dvec3 = vec3<double>;
-	using dvec4 = vec4<double>;
+  // A 3-component vector of float's.
+  using fvec3 = vec3<float>;
 
-	using ivec2 = vec2<int>;
-	using ivec3 = vec3<int>;
-	using ivec4 = vec4<int>;
+  // A 4-component vector of float's.
+  using fvec4 = vec4<float>;
 
-	using uvec2 = vec2<unsigned int>;
-	using uvec3 = vec3<unsigned int>;
-	using uvec4 = vec4<unsigned int>;
+  // A 2-component vector of double's.
+  using dvec2 = vec2<double>;
+
+  // A 3-component vector of double's.
+  using dvec3 = vec3<double>;
+
+  // A 4-component vector of double's.
+  using dvec4 = vec4<double>;
+
+  // A 2-component vector of int's.
+  using ivec2 = vec2<int>;
+
+  // A 3-component vector of int's.
+  using ivec3 = vec3<int>;
+
+  // A 4-component vector of int's.
+  using ivec4 = vec4<int>;
+
+  // A 2-component vector of unsigned int's.
+  using uvec2 = vec2<unsigned int>;
+
+  // A 3-component vector of unsigned int's.
+  using uvec3 = vec3<unsigned int>;
+
+  // A 4-component vector of unsigned int's.
+  using uvec4 = vec4<unsigned int>;
 }
 
 #include "detail_/vec2.hpp"
