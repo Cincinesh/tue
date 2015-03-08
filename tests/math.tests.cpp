ce0daf129215347ce6f0c50b7d3ba264bd6a7612
Jo Bates <cincinesh@icloud.com>
2015-03-08 09:38:54 -0500

    Style change in a couple tests

--- tests/math.tests.cpp
+++ tests/math.tests.cpp
@@ -8,154 +8,138 @@
 
 #include <cmath>
 
+namespace {
 using namespace tue;
 
-namespace
-{
-  TEST_CASE(sincos)
-  {
-    float fsin, fcos;
-    math::sincos(1.23f, fsin, fcos);
-    test_assert(fsin == math::sin(1.23f));
-    test_assert(fcos == math::cos(1.23f));
+TEST_CASE(sincos) {
+  float fsin, fcos;
+  math::sincos(1.23f, fsin, fcos);
+  test_assert(fsin == math::sin(1.23f));
+  test_assert(fcos == math::cos(1.23f));
 
-    double isin, icos;
-    math::sincos(123, isin, icos);
-    test_assert(isin == math::sin(123.0));
-    test_assert(icos == math::cos(123.0));
-  }
+  double isin, icos;
+  math::sincos(123, isin, icos);
+  test_assert(isin == math::sin(123.0));
+  test_assert(icos == math::cos(123.0));
+}
 
-  TEST_CASE(sin)
-  {
-    test_assert(math::sin(1.23f) == std::sin(1.23f));
-    test_assert(math::sin(123) == math::sin(123.0));
-  }
+TEST_CASE(sin) {
+  test_assert(math::sin(1.23f) == std::sin(1.23f));
+  test_assert(math::sin(123) == math::sin(123.0));
+}
 
-  TEST_CASE(cos)
-  {
-    test_assert(math::cos(1.23f) == std::cos(1.23f));
-    test_assert(math::cos(123) == math::cos(123.0));
-  }
+TEST_CASE(cos) {
+  test_assert(math::cos(1.23f) == std::cos(1.23f));
+  test_assert(math::cos(123) == math::cos(123.0));
+}
 
-  TEST_CASE(exp)
-  {
-    test_assert(math::exp(1.23f) == std::exp(1.23f));
-    test_assert(math::exp(123) == math::exp(123.0));
-  }
+TEST_CASE(exp) {
+  test_assert(math::exp(1.23f) == std::exp(1.23f));
+  test_assert(math::exp(123) == math::exp(123.0));
+}
 
-  TEST_CASE(log)
-  {
-    test_assert(math::log(1.23f) == std::log(1.23f));
-    test_assert(math::log(123) == math::log(123.0));
-  }
+TEST_CASE(log) {
+  test_assert(math::log(1.23f) == std::log(1.23f));
+  test_assert(math::log(123) == math::log(123.0));
+}
 
-  TEST_CASE(pow)
-  {
-    test_assert(math::pow(1.23f, 4.56f)
-        == std::pow(1.23f, 4.56f));
-    test_assert(math::pow(1.23f, 4.56)
-        == math::pow(static_cast<double>(1.23f), 4.56));
-    test_assert(math::pow(1.23f, 4.56L)
-        == math::pow(static_cast<long double>(1.23f), 4.56L));
+TEST_CASE(pow) {
+  test_assert(math::pow(1.23f, 4.56f)
+      == std::pow(1.23f, 4.56f));
+  test_assert(math::pow(1.23f, 4.56)
+      == math::pow(static_cast<double>(1.23f), 4.56));
+  test_assert(math::pow(1.23f, 4.56L)
+      == math::pow(static_cast<long double>(1.23f), 4.56L));
 
-    test_assert(math::pow(1.23, 4.56f)
-        == math::pow(1.23, static_cast<double>(4.56f)));
-    test_assert(math::pow(1.23, 4.56)
-        == std::pow(1.23, 4.56));
-    test_assert(math::pow(1.23, 4.56L)
-        == math::pow(static_cast<long double>(1.23), 4.56L));
+  test_assert(math::pow(1.23, 4.56f)
+      == math::pow(1.23, static_cast<double>(4.56f)));
+  test_assert(math::pow(1.23, 4.56)
+      == std::pow(1.23, 4.56));
+  test_assert(math::pow(1.23, 4.56L)
+      == math::pow(static_cast<long double>(1.23), 4.56L));
 
-    test_assert(math::pow(1.23L, 4.56f)
-        == math::pow(1.23L, static_cast<long double>(4.56f)));
-    test_assert(math::pow(1.23L, 4.56)
-        == math::pow(1.23L, static_cast<long double>(4.56)));
-    test_assert(math::pow(1.23L, 4.56L)
-        == std::pow(1.23L, 4.56L));
+  test_assert(math::pow(1.23L, 4.56f)
+      == math::pow(1.23L, static_cast<long double>(4.56f)));
+  test_assert(math::pow(1.23L, 4.56)
+      == math::pow(1.23L, static_cast<long double>(4.56)));
+  test_assert(math::pow(1.23L, 4.56L)
+      == std::pow(1.23L, 4.56L));
 
-    test_assert(math::pow(123, 456)
-        == math::pow(123.0, 456.0));
-    test_assert(math::pow(123, 4.56f)
-        == math::pow(123.0, static_cast<double>(4.56f)));
-    test_assert(math::pow(123, 4.56)
-        == math::pow(123.0, 4.56));
-    test_assert(math::pow(123, 4.56L)
-        == math::pow(123.0L, 4.56L));
+  test_assert(math::pow(123, 456)
+      == math::pow(123.0, 456.0));
+  test_assert(math::pow(123, 4.56f)
+      == math::pow(123.0, static_cast<double>(4.56f)));
+  test_assert(math::pow(123, 4.56)
+      == math::pow(123.0, 4.56));
+  test_assert(math::pow(123, 4.56L)
+      == math::pow(123.0L, 4.56L));
 
-    test_assert(math::pow(1.23f, 456)
-        == math::pow(static_cast<double>(1.23f), 456.0));
-    test_assert(math::pow(1.23, 456)
-        == math::pow(1.23, 456.0));
-    test_assert(math::pow(1.23L, 456)
-        == math::pow(1.23L, 456.0L));
-  }
+  test_assert(math::pow(1.23f, 456)
+      == math::pow(static_cast<double>(1.23f), 456.0));
+  test_assert(math::pow(1.23, 456)
+      == math::pow(1.23, 456.0));
+  test_assert(math::pow(1.23L, 456)
+      == math::pow(1.23L, 456.0L));
+}
 
-  TEST_CASE(recip)
-  {
-    test_assert(math::recip(1.23f) == 1 / 1.23f);
-    test_assert(math::recip(123) == math::recip(123.0));
-  }
+TEST_CASE(recip) {
+  test_assert(math::recip(1.23f) == 1 / 1.23f);
+  test_assert(math::recip(123) == math::recip(123.0));
+}
 
-  TEST_CASE(sqrt)
-  {
-    test_assert(math::sqrt(1.23f) == std::sqrt(1.23f));
-    test_assert(math::sqrt(123) == math::sqrt(123.0));
-  }
+TEST_CASE(sqrt) {
+  test_assert(math::sqrt(1.23f) == std::sqrt(1.23f));
+  test_assert(math::sqrt(123) == math::sqrt(123.0));
+}
 
-  TEST_CASE(rsqrt)
-  {
-    test_assert(math::rsqrt(1.23f) == 1 / std::sqrt(1.23f));
-    test_assert(math::rsqrt(123) == math::rsqrt(123.0));
-  }
+TEST_CASE(rsqrt) {
+  test_assert(math::rsqrt(1.23f) == 1 / std::sqrt(1.23f));
+  test_assert(math::rsqrt(123) == math::rsqrt(123.0));
+}
 
-  TEST_CASE(min)
-  {
-    test_assert(math::min(1.23f, 4.56f) == 1.23f);
-    test_assert(math::min(1.23f, -4.56f) == -4.56f);
-    test_assert(math::min(123, 456) == 123);
-    test_assert(math::min(123, -456) == -456);
-  }
+TEST_CASE(min) {
+  test_assert(math::min(1.23f, 4.56f) == 1.23f);
+  test_assert(math::min(1.23f, -4.56f) == -4.56f);
+  test_assert(math::min(123, 456) == 123);
+  test_assert(math::min(123, -456) == -456);
+}
 
-  TEST_CASE(max)
-  {
-    test_assert(math::max(1.23f, 4.56f) == 4.56f);
-    test_assert(math::max(1.23f, -4.56f) == 1.23f);
-    test_assert(math::max(123, 456) == 456);
-    test_assert(math::max(123, -456) == 123);
-  }
+TEST_CASE(max) {
+  test_assert(math::max(1.23f, 4.56f) == 4.56f);
+  test_assert(math::max(1.23f, -4.56f) == 1.23f);
+  test_assert(math::max(123, 456) == 456);
+  test_assert(math::max(123, -456) == 123);
+}
 
-  TEST_CASE(abs)
-  {
-    test_assert(math::abs(-1.23f) == 1.23f);
-    test_assert(math::abs(1.23f) == 1.23f);
-    test_assert(math::abs(-123) == 123);
-    test_assert(math::abs(123) == 123);
-    test_assert(math::abs(123u) == 123u);
-  }
+TEST_CASE(abs) {
+  test_assert(math::abs(-1.23f) == 1.23f);
+  test_assert(math::abs(1.23f) == 1.23f);
+  test_assert(math::abs(-123) == 123);
+  test_assert(math::abs(123) == 123);
+  test_assert(math::abs(123u) == 123u);
+}
 
-  TEST_CASE(dot)
-  {
-    constexpr auto f = math::dot(1.23f, 4.56);
-    test_assert(f == 1.23f * 4.56);
+TEST_CASE(dot) {
+  constexpr auto f = math::dot(1.23f, 4.56);
+  test_assert(f == 1.23f * 4.56);
 
-    constexpr auto i = math::dot(123, 456);
-    test_assert(i == 123 * 456);
-  }
+  constexpr auto i = math::dot(123, 456);
+  test_assert(i == 123 * 456);
+}
 
-  TEST_CASE(length2)
-  {
-    test_assert(math::length2(-1.23f) == -1.23f * -1.23f);
-    test_assert(math::length2(-123) == math::length2(-123.0));
-  }
+TEST_CASE(length2) {
+  test_assert(math::length2(-1.23f) == -1.23f * -1.23f);
+  test_assert(math::length2(-123) == math::length2(-123.0));
+}
 
-  TEST_CASE(length)
-  {
-    test_assert(math::length(-1.23f) == math::sqrt(math::length2(-1.23f)));
-    test_assert(math::length(-123) == math::length(-123.0));
-  }
+TEST_CASE(length) {
+  test_assert(math::length(-1.23f) == math::sqrt(math::length2(-1.23f)));
+  test_assert(math::length(-123) == math::length(-123.0));
+}
 
-  TEST_CASE(normalize)
-  {
-    test_assert(math::normalize(-1.23f) == -1.23f / math::length(-1.23f));
-    test_assert(math::normalize(-123) == math::normalize(-123.0));
-  }
+TEST_CASE(normalize) {
+  test_assert(math::normalize(-1.23f) == -1.23f / math::length(-1.23f));
+  test_assert(math::normalize(-123) == math::normalize(-123.0));
+}
+
 }
--- tests/mattransform.tests.cpp
+++ tests/mattransform.tests.cpp
@@ -6,205 +6,191 @@
 #include <tue/mat.hpp>
 #include <mon/test_case.hpp>
 
+namespace {
 using namespace tue;
 
-namespace
-{
-  TEST_CASE(translation_mat_2d)
-  {
-    const fmat2x3 m1 = math::translation_mat(1.1f, 2.2f);
-    test_assert(m1[0] == fvec3(1.0f, 0.0f, 1.1f));
-    test_assert(m1[1] == fvec3(0.0f, 1.0f, 2.2f));
+TEST_CASE(translation_mat_2d) {
+  const fmat2x3 m1 = math::translation_mat(1.1f, 2.2f);
+  test_assert(m1[0] == fvec3(1.0f, 0.0f, 1.1f));
+  test_assert(m1[1] == fvec3(0.0f, 1.0f, 2.2f));
 
-    const fmat2x3 m2 = math::translation_mat(fvec2(1.1f, 2.2f));
-    test_assert(m2 == m1);
-  }
+  const fmat2x3 m2 = math::translation_mat(fvec2(1.1f, 2.2f));
+  test_assert(m2 == m1);
+}
 
-  TEST_CASE(translation_mat_3d)
-  {
-    const fmat3x4 m1 = math::translation_mat(1.1f, 2.2f, 3.3f);
-    test_assert(m1[0] == fvec4(1.0f, 0.0f, 0.0f, 1.1f));
-    test_assert(m1[1] == fvec4(0.0f, 1.0f, 0.0f, 2.2f));
-    test_assert(m1[2] == fvec4(0.0f, 0.0f, 1.0f, 3.3f));
+TEST_CASE(translation_mat_3d) {
+  const fmat3x4 m1 = math::translation_mat(1.1f, 2.2f, 3.3f);
+  test_assert(m1[0] == fvec4(1.0f, 0.0f, 0.0f, 1.1f));
+  test_assert(m1[1] == fvec4(0.0f, 1.0f, 0.0f, 2.2f));
+  test_assert(m1[2] == fvec4(0.0f, 0.0f, 1.0f, 3.3f));
 
-    const fmat3x4 m2 = math::translation_mat(fvec3(1.1f, 2.2f, 3.3f));
-    test_assert(m2 == m1);
-  }
+  const fmat3x4 m2 = math::translation_mat(fvec3(1.1f, 2.2f, 3.3f));
+  test_assert(m2 == m1);
+}
 
-  TEST_CASE(rotation_mat_2d)
-  {
-    const fmat2x2 m = math::rotation_mat(1.23f);
-    test_assert(m[0] == fvec2(math::cos(1.23f), -math::sin(1.23f)));
-    test_assert(m[1] == fvec2(math::sin(1.23f),  math::cos(1.23f)));
-  }
+TEST_CASE(rotation_mat_2d) {
+  const fmat2x2 m = math::rotation_mat(1.23f);
+  test_assert(m[0] == fvec2(math::cos(1.23f), -math::sin(1.23f)));
+  test_assert(m[1] == fvec2(math::sin(1.23f),  math::cos(1.23f)));
+}
 
-  TEST_CASE(rotation_mat_from_axis_angle)
-  {
-    const float x = 1.1f;
-    const float y = 2.2f;
-    const float z = 3.3f;
-    const float w = 4.4f;
-    const float s = math::sin(w);
-    const float c = math::cos(w);
-    const fmat3x3 m1 = math::rotation_mat(x, y, z, w);
-    test_assert(m1[0][0] == x*x*(1-c) + c);
-    test_assert(m1[0][1] == y*x*(1-c) - z*s);
-    test_assert(m1[0][2] == x*z*(1-c) + y*s);
-    test_assert(m1[1][0] == x*y*(1-c) + z*s);
-    test_assert(m1[1][1] == y*y*(1-c) + c);
-    test_assert(m1[1][2] == y*z*(1-c) - x*s);
-    test_assert(m1[2][0] == x*z*(1-c) - y*s);
-    test_assert(m1[2][1] == y*z*(1-c) + x*s);
-    test_assert(m1[2][2] == z*z*(1-c) + c);
+TEST_CASE(rotation_mat_from_axis_angle) {
+  const float x = 1.1f;
+  const float y = 2.2f;
+  const float z = 3.3f;
+  const float w = 4.4f;
+  const float s = math::sin(w);
+  const float c = math::cos(w);
+  const fmat3x3 m1 = math::rotation_mat(x, y, z, w);
+  test_assert(m1[0][0] == x*x*(1-c) + c);
+  test_assert(m1[0][1] == y*x*(1-c) - z*s);
+  test_assert(m1[0][2] == x*z*(1-c) + y*s);
+  test_assert(m1[1][0] == x*y*(1-c) + z*s);
+  test_assert(m1[1][1] == y*y*(1-c) + c);
+  test_assert(m1[1][2] == y*z*(1-c) - x*s);
+  test_assert(m1[2][0] == x*z*(1-c) - y*s);
+  test_assert(m1[2][1] == y*z*(1-c) + x*s);
+  test_assert(m1[2][2] == z*z*(1-c) + c);
 
-    const fmat3x3 m2 = math::rotation_mat(fvec3(x, y, z), w);
-    test_assert(m2 == m1);
+  const fmat3x3 m2 = math::rotation_mat(fvec3(x, y, z), w);
+  test_assert(m2 == m1);
 
-    const fmat3x3 m3 = math::rotation_mat(fvec4(x, y, z, w));
-    test_assert(m3 == m1);
-  }
+  const fmat3x3 m3 = math::rotation_mat(fvec4(x, y, z, w));
+  test_assert(m3 == m1);
+}
 
-  TEST_CASE(rotation_mat_from_rotation_vec)
-  {
-    const float x = 1.1f;
-    const float y = 2.2f;
-    const float z = 3.3f;
-    const fmat3x3 m1 = math::rotation_mat(x, y, z);
-    test_assert(m1 == math::rotation_mat(math::axis_angle(x, y, z)));
+TEST_CASE(rotation_mat_from_rotation_vec) {
+  const float x = 1.1f;
+  const float y = 2.2f;
+  const float z = 3.3f;
+  const fmat3x3 m1 = math::rotation_mat(x, y, z);
+  test_assert(m1 == math::rotation_mat(math::axis_angle(x, y, z)));
 
-    const fmat3x3 m2 = math::rotation_mat(fvec3(x, y, z));
-    test_assert(m2 == m1);
-  }
+  const fmat3x3 m2 = math::rotation_mat(fvec3(x, y, z));
+  test_assert(m2 == m1);
+}
 
-  TEST_CASE(rotation_mat_from_quat)
-  {
-    const float x = 1.1f;
-    const float y = 2.2f;
-    const float z = 3.3f;
-    const float w = 4.4f;
-    const fmat3x3 m = math::rotation_mat(fquat(x, y, z, w));
-    test_assert(m[0][0] == 1 - 2*y*y -2*z*z);
-    test_assert(m[0][1] == 2*x*y - 2*z*w);
-    test_assert(m[0][2] == 2*x*z + 2*y*w);
-    test_assert(m[1][0] == 2*x*y + 2*z*w);
-    test_assert(m[1][1] == 1 - 2*x*x - 2*z*z);
-    test_assert(m[1][2] == 2*y*z - 2*x*w);
-    test_assert(m[2][0] == 2*x*z - 2*y*w);
-    test_assert(m[2][1] == 2*y*z + 2*x*w);
-    test_assert(m[2][2] == 1 - 2*x*x - 2*y*y);
-  }
+TEST_CASE(rotation_mat_from_quat) {
+  const float x = 1.1f;
+  const float y = 2.2f;
+  const float z = 3.3f;
+  const float w = 4.4f;
+  const fmat3x3 m = math::rotation_mat(fquat(x, y, z, w));
+  test_assert(m[0][0] == 1 - 2*y*y -2*z*z);
+  test_assert(m[0][1] == 2*x*y - 2*z*w);
+  test_assert(m[0][2] == 2*x*z + 2*y*w);
+  test_assert(m[1][0] == 2*x*y + 2*z*w);
+  test_assert(m[1][1] == 1 - 2*x*x - 2*z*z);
+  test_assert(m[1][2] == 2*y*z - 2*x*w);
+  test_assert(m[2][0] == 2*x*z - 2*y*w);
+  test_assert(m[2][1] == 2*y*z + 2*x*w);
+  test_assert(m[2][2] == 1 - 2*x*x - 2*y*y);
+}
 
-  TEST_CASE(scale_mat_2d)
-  {
-    const fmat2x2 m1 = math::scale_mat(1.1f, 2.2f);
-    test_assert(m1[0] == fvec2(1.1f, 0.0f));
-    test_assert(m1[1] == fvec2(0.0f, 2.2f));
+TEST_CASE(scale_mat_2d) {
+  const fmat2x2 m1 = math::scale_mat(1.1f, 2.2f);
+  test_assert(m1[0] == fvec2(1.1f, 0.0f));
+  test_assert(m1[1] == fvec2(0.0f, 2.2f));
 
-    const fmat2x2 m2 = math::scale_mat(fvec2(1.1f, 2.2f));
-    test_assert(m2 == m1);
-  }
+  const fmat2x2 m2 = math::scale_mat(fvec2(1.1f, 2.2f));
+  test_assert(m2 == m1);
+}
 
-  TEST_CASE(scale_mat_3d)
-  {
-    const fmat3x3 m1 = math::scale_mat(1.1f, 2.2f, 3.3f);
-    test_assert(m1[0] == fvec3(1.1f, 0.0f, 0.0f));
-    test_assert(m1[1] == fvec3(0.0f, 2.2f, 0.0f));
-    test_assert(m1[2] == fvec3(0.0f, 0.0f, 3.3f));
+TEST_CASE(scale_mat_3d) {
+  const fmat3x3 m1 = math::scale_mat(1.1f, 2.2f, 3.3f);
+  test_assert(m1[0] == fvec3(1.1f, 0.0f, 0.0f));
+  test_assert(m1[1] == fvec3(0.0f, 2.2f, 0.0f));
+  test_assert(m1[2] == fvec3(0.0f, 0.0f, 3.3f));
 
-    const fmat3x3 m2 = math::scale_mat(fvec3(1.1f, 2.2f, 3.3f));
-    test_assert(m2 == m1);
-  }
+  const fmat3x3 m2 = math::scale_mat(fvec3(1.1f, 2.2f, 3.3f));
+  test_assert(m2 == m1);
+}
 
-  TEST_CASE(view_mat_2d)
-  {
-    const fvec2 translation(1.1f, 2.2f);
-    const float rotation = 3.3f;
-    const fmat2x3 m = math::view_mat(translation, rotation);
-    test_assert(m
-        == math::translation_mat(-translation)
-            * math::rotation_mat(-rotation));
-  }
+TEST_CASE(view_mat_2d) {
+  const fvec2 translation(1.1f, 2.2f);
+  const float rotation = 3.3f;
+  const fmat2x3 m = math::view_mat(translation, rotation);
+  test_assert(m
+      == math::translation_mat(-translation)
+          * math::rotation_mat(-rotation));
+}
 
-  TEST_CASE(view_mat_from_axis_angle)
-  {
-    const fvec3 translation(1.1f, 2.2f, 3.3f);
-    const fvec3 rotation_axis(4.4f, 5.5f, 6.6f);
-    const float rotation_angle = 7.7f;
-    const fmat3x4 m1 = math::view_mat(
-        translation,
-        rotation_axis,
-        rotation_angle);
-    test_assert(m1
-        == math::translation_mat(-translation)
-            * math::rotation_mat(rotation_axis, -rotation_angle));
+TEST_CASE(view_mat_from_axis_angle) {
+  const fvec3 translation(1.1f, 2.2f, 3.3f);
+  const fvec3 rotation_axis(4.4f, 5.5f, 6.6f);
+  const float rotation_angle = 7.7f;
+  const fmat3x4 m1 = math::view_mat(
+      translation,
+      rotation_axis,
+      rotation_angle);
+  test_assert(m1
+      == math::translation_mat(-translation)
+          * math::rotation_mat(rotation_axis, -rotation_angle));
 
-    const fmat3x4 m2 = math::view_mat(
-        translation,
-        fvec4(rotation_axis, rotation_angle));
-    test_assert(m2 == m1);
-  }
+  const fmat3x4 m2 = math::view_mat(
+      translation,
+      fvec4(rotation_axis, rotation_angle));
+  test_assert(m2 == m1);
+}
 
-  TEST_CASE(view_mat_from_rotation_vec)
-  {
-    const fvec3 translation(1.1f, 2.2f, 3.3f);
-    const fvec3 rotation(4.4f, 5.5f, 6.6f);
-    const fmat3x4 m1 = math::view_mat(translation, rotation);
-    test_assert(m1
-        == math::translation_mat(-translation)
-            * math::rotation_mat(-rotation));
-  }
+TEST_CASE(view_mat_from_rotation_vec) {
+  const fvec3 translation(1.1f, 2.2f, 3.3f);
+  const fvec3 rotation(4.4f, 5.5f, 6.6f);
+  const fmat3x4 m1 = math::view_mat(translation, rotation);
+  test_assert(m1
+      == math::translation_mat(-translation)
+          * math::rotation_mat(-rotation));
+}
 
-  TEST_CASE(view_mat_from_quat)
-  {
-    const fvec3 translation(1.1f, 2.2f, 3.3f);
-    const fquat rotation(4.4f, 5.5f, 6.6f, 7.7f);
-    const fmat3x4 m = math::view_mat(translation, rotation);
-    test_assert(m
-        == math::translation_mat(-translation)
-            * math::rotation_mat(math::conjugate(rotation)));
-  }
+TEST_CASE(view_mat_from_quat) {
+  const fvec3 translation(1.1f, 2.2f, 3.3f);
+  const fquat rotation(4.4f, 5.5f, 6.6f, 7.7f);
+  const fmat3x4 m = math::view_mat(translation, rotation);
+  test_assert(m
+      == math::translation_mat(-translation)
+          * math::rotation_mat(math::conjugate(rotation)));
+}
 
-  TEST_CASE(perspective_mat)
-  {
-    const float fovy = 1.1f;
-    const float aspect = 2.2f;
-    const float near = 3.3f;
-    const float far = 4.4f;
-    const float f = math::cos(fovy/2) / math::sin(fovy/2);
-    const fmat4x4 m = math::perspective_mat(fovy, aspect, near, far);
-    test_assert(m[0][0] == f / aspect);
-    test_assert(m[0][1] == 0.0f);
-    test_assert(m[0][2] == 0.0f);
-    test_assert(m[0][3] == 0.0f);
-    test_assert(m[1][0] == 0.0f);
-    test_assert(m[1][1] == f);
-    test_assert(m[1][2] == 0.0f);
-    test_assert(m[1][3] == 0.0f);
-    test_assert(m[2][0] == 0.0f);
-    test_assert(m[2][1] == 0.0f);
-    test_assert(m[2][2] == (near+far) / (near-far));
-    test_assert(m[2][3] == (2*near*far) / (near-far));
-    test_assert(m[3][0] == 0.0f);
-    test_assert(m[3][1] == 0.0f);
-    test_assert(m[3][2] == -1.0f);
-    test_assert(m[3][3] == 0.0f);
-  }
+TEST_CASE(perspective_mat) {
+  const float fovy = 1.1f;
+  const float aspect = 2.2f;
+  const float near = 3.3f;
+  const float far = 4.4f;
+  const float f = math::cos(fovy/2) / math::sin(fovy/2);
+  const fmat4x4 m = math::perspective_mat(fovy, aspect, near, far);
+  test_assert(m[0][0] == f / aspect);
+  test_assert(m[0][1] == 0.0f);
+  test_assert(m[0][2] == 0.0f);
+  test_assert(m[0][3] == 0.0f);
+  test_assert(m[1][0] == 0.0f);
+  test_assert(m[1][1] == f);
+  test_assert(m[1][2] == 0.0f);
+  test_assert(m[1][3] == 0.0f);
+  test_assert(m[2][0] == 0.0f);
+  test_assert(m[2][1] == 0.0f);
+  test_assert(m[2][2] == (near+far) / (near-far));
+  test_assert(m[2][3] == (2*near*far) / (near-far));
+  test_assert(m[3][0] == 0.0f);
+  test_assert(m[3][1] == 0.0f);
+  test_assert(m[3][2] == -1.0f);
+  test_assert(m[3][3] == 0.0f);
+}
 
-  TEST_CASE(ortho_mat)
-  {
-    const float width = 1.1f;
-    const float height = 2.2f;
-    const float near = 3.3f;
-    const float far = 4.4f;
-    const fmat3x3 m = math::ortho_mat(width, height, near, far);
-    test_assert(m[0][0] == 2 / width);
-    test_assert(m[0][1] == 0.0f);
-    test_assert(m[0][2] == 0.0f);
-    test_assert(m[1][0] == 0.0f);
-    test_assert(m[1][1] == 2 / height);
-    test_assert(m[1][2] == 0.0f);
-    test_assert(m[2][0] == 0.0f);
-    test_assert(m[2][1] == 0.0f);
-    test_assert(m[2][2] == 2 / (near - far));
-  }
+TEST_CASE(ortho_mat) {
+  const float width = 1.1f;
+  const float height = 2.2f;
+  const float near = 3.3f;
+  const float far = 4.4f;
+  const fmat3x3 m = math::ortho_mat(width, height, near, far);
+  test_assert(m[0][0] == 2 / width);
+  test_assert(m[0][1] == 0.0f);
+  test_assert(m[0][2] == 0.0f);
+  test_assert(m[1][0] == 0.0f);
+  test_assert(m[1][1] == 2 / height);
+  test_assert(m[1][2] == 0.0f);
+  test_assert(m[2][0] == 0.0f);
+  test_assert(m[2][1] == 0.0f);
+  test_assert(m[2][2] == 2 / (near - far));
+}
+
 }
