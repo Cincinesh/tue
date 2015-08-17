379ca97d52b5c6c19594bfdf70158812accc318e
Jo Bates <cincinesh@icloud.com>
2015-08-16 19:12:07 -0500

    Converted math.hpp to use std::enable_if_t

--- include/tue/math.hpp
+++ include/tue/math.hpp
@@ -20,185 +20,185 @@ namespace math
   constexpr float pi = 3.14159265358979323846264338327950288f;
 
   template<typename T>
-  inline typename std::enable_if<std::is_floating_point<T>::value,
-  T>::type sin(T x) noexcept {
+  inline std::enable_if_t<std::is_floating_point<T>::value,
+  T> sin(T x) noexcept {
     return std::sin(x);
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_integral<T>::value,
-  double>::type sin(T x) noexcept {
+  inline std::enable_if_t<std::is_integral<T>::value,
+  double> sin(T x) noexcept {
     return math::sin(static_cast<double>(x));
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_floating_point<T>::value,
-  T>::type cos(T x) noexcept {
+  inline std::enable_if_t<std::is_floating_point<T>::value,
+  T> cos(T x) noexcept {
     return std::cos(x);
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_integral<T>::value,
-  double>::type cos(T x) noexcept {
+  inline std::enable_if_t<std::is_integral<T>::value,
+  double> cos(T x) noexcept {
     return math::cos(static_cast<double>(x));
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_floating_point<T>::value,
-  void>::type sincos(T x, T& sin_out, T& cos_out) noexcept {
+  inline std::enable_if_t<std::is_floating_point<T>::value,
+  void> sincos(T x, T& sin_out, T& cos_out) noexcept {
     sin_out = std::sin(x);
     cos_out = std::cos(x);
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_integral<T>::value,
-  void>::type sincos(T x, double& sin_out, double& cos_out) noexcept {
+  inline std::enable_if_t<std::is_integral<T>::value,
+  void> sincos(T x, double& sin_out, double& cos_out) noexcept {
     math::sincos(static_cast<double>(x), sin_out, cos_out);
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_floating_point<T>::value,
-  T>::type exp(T x) noexcept {
+  inline std::enable_if_t<std::is_floating_point<T>::value,
+  T> exp(T x) noexcept {
     return std::exp(x);
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_integral<T>::value,
-  double>::type exp(T x) noexcept {
+  inline std::enable_if_t<std::is_integral<T>::value,
+  double> exp(T x) noexcept {
     return math::exp(static_cast<double>(x));
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_floating_point<T>::value,
-  T>::type log(T x) noexcept {
+  inline std::enable_if_t<std::is_floating_point<T>::value,
+  T> log(T x) noexcept {
     return std::log(x);
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_integral<T>::value,
-  double>::type log(T x) noexcept {
+  inline std::enable_if_t<std::is_integral<T>::value,
+  double> log(T x) noexcept {
     return math::log(static_cast<double>(x));
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_floating_point<T>::value,
-  T>::type pow(T base, T exponent) noexcept {
+  inline std::enable_if_t<std::is_floating_point<T>::value,
+  T> pow(T base, T exponent) noexcept {
     return std::pow(base, exponent);
   }
 
   template<typename T, typename U>
-  inline typename std::enable_if<
+  inline std::enable_if_t<
       std::is_arithmetic<T>::value
       && std::is_arithmetic<U>::value
       && !std::is_same<T, long double>::value
       && !std::is_same<U, long double>::value,
-  double>::type pow(T base, U exponent) noexcept {
+  double> pow(T base, U exponent) noexcept {
     return math::pow(
         static_cast<double>(base),
         static_cast<double>(exponent));
   }
 
   template<typename T, typename U>
-  inline typename std::enable_if<
+  inline std::enable_if_t<
       std::is_arithmetic<T>::value
       && std::is_arithmetic<U>::value
       && (std::is_same<T, long double>::value
           ^ std::is_same<U, long double>::value),
-  long double>::type pow(T base, U exponent) noexcept {
+  long double> pow(T base, U exponent) noexcept {
     return math::pow(
         static_cast<long double>(base),
         static_cast<long double>(exponent));
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_floating_point<T>::value,
-  T>::type recip(T x) noexcept {
+  inline std::enable_if_t<std::is_floating_point<T>::value,
+  T> recip(T x) noexcept {
     return 1 / x;
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_integral<T>::value,
-  double>::type recip(T x) noexcept {
+  inline std::enable_if_t<std::is_integral<T>::value,
+  double> recip(T x) noexcept {
     return math::recip(static_cast<double>(x));
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_floating_point<T>::value,
-  T>::type sqrt(T x) noexcept {
+  inline std::enable_if_t<std::is_floating_point<T>::value,
+  T> sqrt(T x) noexcept {
     return std::sqrt(x);
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_integral<T>::value,
-  double>::type sqrt(T x) noexcept {
+  inline std::enable_if_t<std::is_integral<T>::value,
+  double> sqrt(T x) noexcept {
     return math::sqrt(static_cast<double>(x));
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_floating_point<T>::value,
-  T>::type rsqrt(T x) noexcept {
+  inline std::enable_if_t<std::is_floating_point<T>::value,
+  T> rsqrt(T x) noexcept {
     return math::recip(math::sqrt(x));
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_integral<T>::value,
-  double>::type rsqrt(T x) noexcept {
+  inline std::enable_if_t<std::is_integral<T>::value,
+  double> rsqrt(T x) noexcept {
     return math::rsqrt(static_cast<double>(x));
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_arithmetic<T>::value,
-  T>::type min(T x1, T x2) noexcept {
+  inline std::enable_if_t<std::is_arithmetic<T>::value,
+  T> min(T x1, T x2) noexcept {
     return std::min(x1, x2);
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_arithmetic<T>::value,
-  T>::type max(T x1, T x2) noexcept {
+  inline std::enable_if_t<std::is_arithmetic<T>::value,
+  T> max(T x1, T x2) noexcept {
     return std::max(x1, x2);
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_signed<T>::value,
-  T>::type abs(T x) noexcept {
+  inline std::enable_if_t<std::is_signed<T>::value,
+  T> abs(T x) noexcept {
     return static_cast<T>(std::abs(x));
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_unsigned<T>::value,
-  T>::type abs(T x) noexcept {
+  inline std::enable_if_t<std::is_unsigned<T>::value,
+  T> abs(T x) noexcept {
     return x;
   }
 
   template<typename T, typename U>
-  inline constexpr typename std::enable_if<
+  inline constexpr std::enable_if_t<
       std::is_arithmetic<T>::value
       && std::is_arithmetic<U>::value,
-  decltype(T() * U())>::type dot(T lhs, U rhs) noexcept {
+  decltype(T() * U())> dot(T lhs, U rhs) noexcept {
     return lhs * rhs;
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_floating_point<T>::value,
-  T>::type length2(T x) noexcept {
+  inline std::enable_if_t<std::is_floating_point<T>::value,
+  T> length2(T x) noexcept {
     return x * x;
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_integral<T>::value,
-  double>::type length2(T x) noexcept {
+  inline std::enable_if_t<std::is_integral<T>::value,
+  double> length2(T x) noexcept {
     return math::length2(static_cast<double>(x));
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_floating_point<T>::value,
-  T>::type length(T x) noexcept {
+  inline std::enable_if_t<std::is_floating_point<T>::value,
+  T> length(T x) noexcept {
     return math::abs(x);
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_integral<T>::value,
-  double>::type length(T x) noexcept {
+  inline std::enable_if_t<std::is_integral<T>::value,
+  double> length(T x) noexcept {
     return math::length(static_cast<double>(x));
   }
 
@@ -215,22 +215,22 @@ namespace math
   }
 
   template<typename T>
-  inline typename std::enable_if<std::is_integral<T>::value,
-  double>::type normalize(T x) noexcept {
+  inline std::enable_if_t<std::is_integral<T>::value,
+  double> normalize(T x) noexcept {
     return math::normalize(static_cast<double>(x));
   }
 
   template<typename T, typename U>
-  inline constexpr typename std::enable_if<
+  inline constexpr std::enable_if_t<
       std::is_arithmetic<T>::value
       && std::is_arithmetic<U>::value,
-  decltype(T() * U())>::type comp_mult(T lhs, U rhs) noexcept {
+  decltype(T() * U())> comp_mult(T lhs, U rhs) noexcept {
     return lhs * rhs;
   }
 
   template<typename T>
-  inline constexpr typename std::enable_if<std::is_arithmetic<T>::value,
-  T>::type transpose(T x) noexcept {
+  inline constexpr std::enable_if_t<std::is_arithmetic<T>::value,
+  T> transpose(T x) noexcept {
     return x;
   }
 
@@ -243,50 +243,50 @@ namespace math
   }
 
   template<typename T, typename U>
-  inline constexpr typename std::enable_if<
+  inline constexpr std::enable_if_t<
       std::is_arithmetic<T>::value
       && std::is_arithmetic<U>::value,
-  bool>::type less(T lhs, U rhs) noexcept {
+  bool> less(T lhs, U rhs) noexcept {
     return lhs < rhs;
   }
 
   template<typename T, typename U>
-  inline constexpr typename std::enable_if<
+  inline constexpr std::enable_if_t<
       std::is_arithmetic<T>::value
       && std::is_arithmetic<U>::value,
-  bool>::type less_equal(T lhs, U rhs) noexcept {
+  bool> less_equal(T lhs, U rhs) noexcept {
     return lhs <= rhs;
   }
 
   template<typename T, typename U>
-  inline constexpr typename std::enable_if<
+  inline constexpr std::enable_if_t<
       std::is_arithmetic<T>::value
       && std::is_arithmetic<U>::value,
-  bool>::type greater(T lhs, U rhs) noexcept {
+  bool> greater(T lhs, U rhs) noexcept {
     return lhs > rhs;
   }
 
   template<typename T, typename U>
-  inline constexpr typename std::enable_if<
+  inline constexpr std::enable_if_t<
       std::is_arithmetic<T>::value
       && std::is_arithmetic<U>::value,
-  bool>::type greater_equal(T lhs, U rhs) noexcept {
+  bool> greater_equal(T lhs, U rhs) noexcept {
     return lhs >= rhs;
   }
 
   template<typename T, typename U>
-  inline constexpr typename std::enable_if<
+  inline constexpr std::enable_if_t<
       std::is_arithmetic<T>::value
       && std::is_arithmetic<U>::value,
-  bool>::type equal(T lhs, U rhs) noexcept {
+  bool> equal(T lhs, U rhs) noexcept {
     return lhs == rhs;
   }
 
   template<typename T, typename U>
-  inline constexpr typename std::enable_if<
+  inline constexpr std::enable_if_t<
       std::is_arithmetic<T>::value
       && std::is_arithmetic<U>::value,
-  bool>::type not_equal(T lhs, U rhs) noexcept {
+  bool> not_equal(T lhs, U rhs) noexcept {
     return lhs != rhs;
   }
 }
