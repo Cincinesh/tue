f8df223ad4a7d3adb70d2fbc9ea2a2a73584bb5c
Jo Bates <cincinesh@icloud.com>
2015-08-27 19:07:02 -0500

    Combined related tests

--- tests/math.tests.cpp
+++ tests/math.tests.cpp
@@ -15,73 +15,49 @@ namespace
 {
     using namespace tue;
 
-    TEST_CASE(floating_point_cos)
+    TEST_CASE(cos)
     {
         test_assert(math::cos(1.2) == std::cos(1.2));
-    }
-
-    TEST_CASE(integral_cos)
-    {
         test_assert(math::cos(12) == std::cos(12.0));
     }
 
-
-    TEST_CASE(floating_point_sin)
+    TEST_CASE(sin)
     {
         test_assert(math::sin(1.2) == std::sin(1.2));
-    }
-
-    TEST_CASE(integral_sin)
-    {
         test_assert(math::sin(12) == std::sin(12.0));
     }
 
-    TEST_CASE(floating_point_sincos)
+    TEST_CASE(sincos)
     {
         double s, c;
+
         math::sincos(1.2, s, c);
         test_assert(s == std::sin(1.2));
         test_assert(c == std::cos(1.2));
-    }
 
-    TEST_CASE(integral_sincos)
-    {
-        double s, c;
         math::sincos(12, s, c);
         test_assert(s == std::sin(12.0));
         test_assert(c == std::cos(12.0));
     }
 
-    TEST_CASE(floating_point_exp)
+    TEST_CASE(exp)
     {
         test_assert(math::exp(1.2) == std::exp(1.2));
-    }
-
-    TEST_CASE(integral_exp)
-    {
         test_assert(math::exp(12) == std::exp(12.0));
     }
 
-    TEST_CASE(floating_point_log)
+    TEST_CASE(log)
     {
         test_assert(math::log(1.2) == std::log(1.2));
-    }
-
-    TEST_CASE(integral_log)
-    {
         test_assert(math::log(12) == std::log(12.0));
     }
 
-    TEST_CASE(signed_abs)
+    TEST_CASE(abs)
     {
         test_assert(math::abs(1.2) == 1.2);
         test_assert(math::abs(-1.2) == 1.2);
         test_assert(math::abs(12) == 12);
         test_assert(math::abs(-12) == 12);
-    }
-
-    TEST_CASE(unsigned_abs)
-    {
         test_assert(math::abs(12u) == 12u);
     }
 
@@ -100,33 +76,21 @@ namespace
         test_assert(math::pow(12, 34) == std::pow(12.0, 34.0));
     }
 
-    TEST_CASE(floating_point_recip)
+    TEST_CASE(recip)
     {
         test_assert(math::recip(1.2) == 1 / 1.2);
-    }
-
-    TEST_CASE(integral_recip)
-    {
         test_assert(math::recip(12) == 1 / 12.0);
     }
 
-    TEST_CASE(floating_point_rsqrt)
+    TEST_CASE(rsqrt)
     {
         test_assert(math::rsqrt(1.2) == 1 / std::sqrt(1.2));
-    }
-
-    TEST_CASE(integral_rsqrt)
-    {
         test_assert(math::rsqrt(12) == 1 / std::sqrt(12.0));
     }
 
-    TEST_CASE(floating_point_sqrt)
+    TEST_CASE(sqrt)
     {
         test_assert(math::sqrt(1.2) == std::sqrt(1.2));
-    }
-
-    TEST_CASE(integral_sqrt)
-    {
         test_assert(math::sqrt(12) == std::sqrt(12.0));
     }
 
