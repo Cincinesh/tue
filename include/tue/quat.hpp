54d4809b468b4137bedc3a612769d7aca8b31444
Jo Bates <cincinesh@icloud.com>
2015-08-30 11:06:13 -0500

    Added a quat constructor and some getters

--- include/tue/quat.hpp
+++ include/tue/quat.hpp
@@ -49,5 +49,68 @@ namespace tue
          * \brief  Default constructs each component.
          */
         quat() noexcept = default;
+
+        /*!
+         * \brief    Constructs each component with the value of the
+         *           corresponding argument.
+         *
+         * \param x  The value to construct the first component with.
+         * \param y  The value to construct the second component with.
+         * \param z  The value to construct the third component with.
+         * \param w  The value to construct the fourth component with.
+         */
+        constexpr quat(
+            const T& x, const T& y, const T& z, const T& w) noexcept :
+            impl_({{ x, y, z, w }})
+        {
+        }
+
+        /*!
+         * \brief   Returns a pointer to this `quat`'s underlying component
+         *          array.
+         *
+         * \return  A pointer to this `quat`'s underlying component array.
+         */
+        const T* data() const noexcept
+        {
+            return this->impl_.data;
+        }
+
+        /*!
+         * \brief   Returns a pointer to this `quat`'s underlying component
+         *          array.
+         *
+         * \return  A pointer to this `quat`'s underlying component array.
+         */
+        T* data() noexcept
+        {
+            return this->impl_.data;
+        }
+
+        /*!
+         * \brief     Returns a reference to the component at the given index.
+         * \details   No bounds checking is performed.
+         * \tparam I  The index type.
+         * \param i   The index.
+         * \return    A reference to the component at the given index.
+         */
+        template<typename I>
+        constexpr const T& operator[](const I& i) const noexcept
+        {
+            return this->impl_.data[i];
+        }
+
+        /*!
+         * \brief     Returns a reference to the component at the given index.
+         * \details   No bounds checking is performed.
+         * \tparam I  The index type.
+         * \param i   The index.
+         * \return    A reference to the component at the given index.
+         */
+        template<typename I>
+        T& operator[](const I& i) noexcept
+        {
+            return this->impl_.data[i];
+        }
     };
 }
--- tests/quat.tests.cpp
+++ tests/quat.tests.cpp
@@ -51,4 +51,55 @@ namespace
         dquat q;
         unused(q);
     }
+
+    TEST_CASE(individual_components_constructor)
+    {
+        CONST_OR_CONSTEXPR dquat v = { 1.2, 3.4, 5.6, 7.8 };
+        test_assert(v[0] == 1.2);
+        test_assert(v[1] == 3.4);
+        test_assert(v[2] == 5.6);
+        test_assert(v[3] == 7.8);
+    }
+
+    TEST_CASE(data)
+    {
+        const dquat dq(1.2, 3.4, 5.6, 7.8);
+        test_assert(dq.data() == static_cast<const void*>(&dq));
+
+        dquat q;
+        test_assert(q.data() == static_cast<void*>(&q));
+    }
+
+    TEST_CASE(subscript_operator)
+    {
+        CONST_OR_CONSTEXPR dquat ceq(1.2, 3.4, 5.6, 7.8);
+        CONST_OR_CONSTEXPR auto ceq0 = ceq[0];
+        CONST_OR_CONSTEXPR auto ceq1 = ceq[1];
+        CONST_OR_CONSTEXPR auto ceq2 = ceq[2];
+        CONST_OR_CONSTEXPR auto ceq3 = ceq[3];
+        test_assert(ceq0 == 1.2);
+        test_assert(ceq1 == 3.4);
+        test_assert(ceq2 == 5.6);
+        test_assert(ceq3 == 7.8);
+
+        const auto cq = ceq;
+        const auto& cq0 = cq[0];
+        const auto& cq1 = cq[1];
+        const auto& cq2 = cq[2];
+        const auto& cq3 = cq[3];
+        test_assert(&cq0 == cq.data() + 0);
+        test_assert(&cq1 == cq.data() + 1);
+        test_assert(&cq2 == cq.data() + 2);
+        test_assert(&cq3 == cq.data() + 3);
+
+        auto q = ceq;
+        auto& q0 = q[0];
+        auto& q1 = q[1];
+        auto& q2 = q[2];
+        auto& q3 = q[3];
+        test_assert(&q0 == q.data() + 0);
+        test_assert(&q1 == q.data() + 1);
+        test_assert(&q2 == q.data() + 2);
+        test_assert(&q3 == q.data() + 3);
+    }
 }
