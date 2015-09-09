20b6bfb1abe3e813cff7b9c2d90df6239c75acef
Jo Bates <cincinesh@icloud.com>
2015-09-09 07:48:02 -0500

    Changed the ordering of some declarations and reduces code duplication

--- include/tue/transform.hpp
+++ include/tue/transform.hpp
@@ -30,29 +30,6 @@ namespace tue
          *            `(0, 0, 1, 0)`.
          *
          * \tparam T  The rotation vector's component type.
-         * \param v   The rotation vector.
-         * \return    The axis-angle vector.
-         */
-        template<typename T>
-        inline vec4<decltype(tue::math::sqrt(std::declval<T>()))>
-        axis_angle(const vec3<T>& v) noexcept
-        {
-            using U = decltype(tue::math::length(v));
-            const auto angle = tue::math::length(v);
-            const auto axis = tue::math::select(
-                tue::math::not_equal(angle, U(0)),
-                v / angle,
-                vec3<U>::z_axis());
-
-            return { axis, angle };
-        }
-
-        /*!
-         * \brief     Converts a rotation vector to an axis-angle vector.
-         * \details   If the rotation vector's length is `0`, returns
-         *            `(0, 0, 1, 0)`.
-         *
-         * \tparam T  The rotation vector's component type.
          *
          * \param x   The rotation vector's first component.
          * \param y   The rotation vector's second component.
@@ -64,24 +41,30 @@ namespace tue
         inline vec4<decltype(tue::math::sqrt(std::declval<T>()))>
         axis_angle(const T& x, const T& y, const T& z) noexcept
         {
-            return tue::transform::axis_angle(vec3<T>(x, y, z));
+            const auto angle = tue::math::sqrt(x*x + y*y + z*z);
+            using U = decltype(angle);
+
+            const auto nzmask = tue::math::not_equal(angle, U(0));
+            const auto axis_x = tue::math::select(nzmask, x / angle, U(0));
+            const auto axis_y = tue::math::select(nzmask, y / angle, U(0));
+            const auto axis_z = tue::math::select(nzmask, z / angle, U(1));
+            return { axis_x, axis_y, axis_z, angle };
         }
 
         /*!
-         * \brief        Converts an axis-angle pair to a rotation vector.
+         * \brief     Converts a rotation vector to an axis-angle vector.
+         * \details   If the rotation vector's length is `0`, returns
+         *            `(0, 0, 1, 0)`.
          *
-         * \tparam T     The axis-angle component type.
-         *
-         * \param axis   The axis.
-         * \param angle  The angle.
-         *
-         * \return       The rotation vector.
+         * \tparam T  The rotation vector's component type.
+         * \param v   The rotation vector.
+         * \return    The axis-angle vector.
          */
         template<typename T>
-        inline constexpr vec3<T>
-        rotation_vec(const vec3<T>& axis, const T& angle) noexcept
+        inline vec4<decltype(tue::math::sqrt(std::declval<T>()))>
+        axis_angle(const vec3<T>& v) noexcept
         {
-            return axis * angle;
+            return tue::transform::axis_angle(v[0], v[1], v[2]);
         }
 
         /*!
@@ -110,6 +93,24 @@ namespace tue
         }
 
         /*!
+         * \brief        Converts an axis-angle pair to a rotation vector.
+         *
+         * \tparam T     The axis-angle component type.
+         *
+         * \param axis   The axis.
+         * \param angle  The angle.
+         *
+         * \return       The rotation vector.
+         */
+        template<typename T>
+        inline constexpr vec3<T>
+        rotation_vec(const vec3<T>& axis, const T& angle) noexcept
+        {
+            return tue::transform::rotation_vec(
+                axis[0], axis[1], axis[2], angle);
+        }
+
+        /*!
          * \brief     Converts an axis-angle vector to a rotation vector.
          * \tparam T  The axis-angle component type.
          * \param v   The axis-angle vector.
@@ -119,31 +120,8 @@ namespace tue
         inline constexpr vec3<T>
         rotation_vec(const vec4<T>& v) noexcept
         {
-            return {
-                v[0] * v[3],
-                v[1] * v[3],
-                v[2] * v[3],
-            };
-        }
-
-        /*!
-         * \brief        Converts an axis-angle pair to a rotation quaternion.
-         *
-         * \tparam T     The axis-angle component type.
-         *
-         * \param axis   The axis.
-         * \param angle  The angle.
-         *
-         * \return       The rotation quaternion.
-         */
-        template<typename T>
-        inline quat<decltype(tue::math::sin(std::declval<T>()))>
-        rotation_quat(const vec3<T>& axis, const T& angle) noexcept
-        {
-            using U = decltype(tue::math::sin(angle));
-            U s, c;
-            tue::math::sincos(angle / U(2), s, c);
-            return { axis[0]*s, axis[1]*s, axis[2]*s, c };
+            return tue::transform::rotation_vec(
+                v[0], v[1], v[2], v[3]);
         }
 
         /*!
@@ -171,6 +149,24 @@ namespace tue
         }
 
         /*!
+         * \brief        Converts an axis-angle pair to a rotation quaternion.
+         *
+         * \tparam T     The axis-angle component type.
+         *
+         * \param axis   The axis.
+         * \param angle  The angle.
+         *
+         * \return       The rotation quaternion.
+         */
+        template<typename T>
+        inline quat<decltype(tue::math::sin(std::declval<T>()))>
+        rotation_quat(const vec3<T>& axis, const T& angle) noexcept
+        {
+            return tue::transform::rotation_quat(
+                axis[0], axis[1], axis[2], angle);
+        }
+
+        /*!
          * \brief     Converts an axis-angle vector to a rotation quaternion.
          * \tparam T  The axis-angle component type.
          * \param v   The axis-angle vector.
@@ -180,27 +176,8 @@ namespace tue
         inline quat<decltype(tue::math::sin(std::declval<T>()))>
         rotation_quat(const vec4<T>& v) noexcept
         {
-            using U = decltype(tue::math::sin(v[3]));
-            U s, c;
-            tue::math::sincos(v[3] / U(2), s, c);
-            return { v[0]*s, v[1]*s, v[2]*s, c };
-        }
-
-        /*!
-         * \brief     Converts a rotation vector to a rotation quaternion.
-         * \details   If the rotation vector's length is `0`, returns
-         *            `(0, 0, 0, 1)`.
-         *
-         * \tparam T  The rotation vector's component type.
-         * \param v   The rotation vector.
-         * \return    The rotation quaternion.
-         */
-        template<typename T>
-        inline quat<decltype(tue::math::sin(std::declval<T>()))>
-        rotation_quat(const vec3<T>& v) noexcept
-        {
             return tue::transform::rotation_quat(
-                tue::transform::axis_angle(v));
+                v[0], v[1], v[2], v[3]);
         }
 
         /*!
@@ -222,6 +199,23 @@ namespace tue
         {
             return tue::transform::rotation_quat(
                 tue::transform::axis_angle(x, y, z));
+        }
+
+        /*!
+         * \brief     Converts a rotation vector to a rotation quaternion.
+         * \details   If the rotation vector's length is `0`, returns
+         *            `(0, 0, 0, 1)`.
+         *
+         * \tparam T  The rotation vector's component type.
+         * \param v   The rotation vector.
+         * \return    The rotation quaternion.
+         */
+        template<typename T>
+        inline quat<decltype(tue::math::sin(std::declval<T>()))>
+        rotation_quat(const vec3<T>& v) noexcept
+        {
+            return tue::transform::rotation_quat(
+                tue::transform::axis_angle(v));
         }
 
         /*!
--- tests/transform.tests.cpp
+++ tests/transform.tests.cpp
@@ -19,38 +19,34 @@ namespace
 
     TEST_CASE(rotation_vec_to_axis_angle)
     {
-        const ivec3 rv1(1, 2, 3);
-        const auto aa1 = transform::axis_angle(rv1);
-        test_assert(aa1.xyz() == math::normalize(rv1));
-        test_assert(aa1.a() == math::length(rv1));
+        const auto aa1 = transform::axis_angle(1, 2, 3);
+        test_assert(aa1.xyz() == math::normalize(ivec3(1, 2, 3)));
+        test_assert(aa1.a() == math::length(ivec3(1, 2, 3)));
 
-        const ivec3 rv2(0, 0, 0);
-        const auto aa2 = transform::axis_angle(rv2);
+        const auto aa2 = transform::axis_angle(0, 0, 0);
         test_assert(aa2.xyz() == dvec3::z_axis());
         test_assert(aa2.a() == 0.0);
 
-        const auto aa3 = transform::axis_angle(1, 2, 3);
-        test_assert(aa3.xyz() == math::normalize(rv1));
-        test_assert(aa3.a() == math::length(rv1));
+        const auto aa3 = transform::axis_angle(ivec3(1, 2, 3));
+        test_assert(aa3 == aa1);
 
-        const auto aa4 = transform::axis_angle(0, 0, 0);
-        test_assert(aa4.xyz() == dvec3::z_axis());
-        test_assert(aa4.a() == 0.0);
+        const auto aa4 = transform::axis_angle(ivec3(0, 0, 0));
+        test_assert(aa4 == aa2);
     }
 
     TEST_CASE(axis_angle_to_rotation_vec)
     {
         CONST_OR_CONSTEXPR auto rv1 =
-            transform::rotation_vec(dvec3(1.2, 3.4, 5.6), 7.8);
+            transform::rotation_vec(1.2, 3.4, 5.6, 7.8);
         test_assert(rv1 == dvec3(1.2, 3.4, 5.6) * 7.8);
 
         CONST_OR_CONSTEXPR auto rv2 =
-            transform::rotation_vec(1.2, 3.4, 5.6, 7.8);
-        test_assert(rv2 == dvec3(1.2, 3.4, 5.6) * 7.8);
+            transform::rotation_vec(dvec3(1.2, 3.4, 5.6), 7.8);
+        test_assert(rv2 == rv1);
 
         CONST_OR_CONSTEXPR auto rv3 =
             transform::rotation_vec(dvec4(1.2, 3.4, 5.6, 7.8));
-        test_assert(rv3 == dvec3(1.2, 3.4, 5.6) * 7.8);
+        test_assert(rv3 == rv1);
     }
 
     TEST_CASE(axis_angle_to_rotation_quat)
@@ -60,29 +56,26 @@ namespace
         test_assert(rq1.s() == math::cos(5/2.0));
 
         const auto rq2 = transform::rotation_quat(1, 2, 3, 5);
-        test_assert(rq2.v() == ivec3(1, 2, 3) * math::sin(5/2.0));
-        test_assert(rq2.s() == math::cos(5/2.0));
+        test_assert(rq2 == rq1);
 
         const auto rq3 = transform::rotation_quat(ivec4(1, 2, 3, 5));
-        test_assert(rq3.v() == ivec3(1, 2, 3) * math::sin(5/2.0));
-        test_assert(rq3.s() == math::cos(5/2.0));
+        test_assert(rq3 == rq1);
     }
 
     TEST_CASE(rotation_vec_to_rotation_quat)
     {
-        const auto rq1 = transform::rotation_quat(ivec3(1, 2, 3));
+        const auto rq1 = transform::rotation_quat(1, 2, 3);
         test_assert(rq1 ==
             transform::rotation_quat(transform::axis_angle(1, 2, 3)));
 
-        const auto rq2 = transform::rotation_quat(ivec3(0, 0, 0));
+        const auto rq2 = transform::rotation_quat(0, 0, 0);
         test_assert(rq2 == dquat::identity());
 
-        const auto rq3 = transform::rotation_quat(1, 2, 3);
-        test_assert(rq3 ==
-            transform::rotation_quat(transform::axis_angle(1, 2, 3)));
+        const auto rq3 = transform::rotation_quat(ivec3(1, 2, 3));
+        test_assert(rq3 == rq1);
 
-        const auto rq4 = transform::rotation_quat(0, 0, 0);
-        test_assert(rq4 == dquat::identity());
+        const auto rq4 = transform::rotation_quat(ivec3(0, 0, 0));
+        test_assert(rq4 == rq2);
     }
 
     TEST_CASE(translation_mat_2d)
@@ -101,15 +94,11 @@ namespace
 
         CONST_OR_CONSTEXPR auto m3 =
             transform::translation_mat(dvec2(1.2, 3.4));
-        test_assert(m3[0] == dvec4(1.0, 0.0, 0.0, 1.2));
-        test_assert(m3[1] == dvec4(0.0, 1.0, 0.0, 3.4));
-        test_assert(m3[2] == dvec4(0.0, 0.0, 1.0, 0.0));
-        test_assert(m3[3] == dvec4(0.0, 0.0, 0.0, 1.0));
+        test_assert(m3 == m1);
 
         CONST_OR_CONSTEXPR auto m4 =
             transform::translation_mat<double, 2, 3>(dvec2(1.2, 3.4));
-        test_assert(m4[0] == dvec3(1.0, 0.0, 1.2));
-        test_assert(m4[1] == dvec3(0.0, 1.0, 3.4));
+        test_assert(m4 == m2);
     }
 
     TEST_CASE(translation_mat_3d)
@@ -129,16 +118,11 @@ namespace
 
         CONST_OR_CONSTEXPR auto m3 =
             transform::translation_mat(dvec3(1.2, 3.4, 5.6));
-        test_assert(m3[0] == dvec4(1.0, 0.0, 0.0, 1.2));
-        test_assert(m3[1] == dvec4(0.0, 1.0, 0.0, 3.4));
-        test_assert(m3[2] == dvec4(0.0, 0.0, 1.0, 5.6));
-        test_assert(m3[3] == dvec4(0.0, 0.0, 0.0, 1.0));
+        test_assert(m3 == m1);
 
         CONST_OR_CONSTEXPR auto m4 =
             transform::translation_mat<double, 3, 4>(dvec3(1.2, 3.4, 5.6));
-        test_assert(m4[0] == dvec4(1.0, 0.0, 0.0, 1.2));
-        test_assert(m4[1] == dvec4(0.0, 1.0, 0.0, 3.4));
-        test_assert(m4[2] == dvec4(0.0, 0.0, 1.0, 5.6));
+        test_assert(m4 == m2);
     }
 
     TEST_CASE(rotation_mat_2d)
