834c90fe3584f0ce4068366f9f1fbd4e067db70a
Jo Bates <cincinesh@icloud.com>
2015-10-09 08:19:56 -0500

    Added SIMD support macro documentation

--- include/tue/detail_/simd_support.hpp
+++ include/tue/detail_/simd_support.hpp
@@ -8,14 +8,28 @@
 
 #pragma once
 
+/*!
+ * \addtogroup  simd_hpp
+ * @{
+ */
 #if defined(__SSE__) \
     || (defined(_M_IX86_FP) && _M_IX86_FP >= 1) \
     || defined(_M_X64)
+/*!
+ * \brief Defined if the current compiler configuration supports SSE
+ *        intrinsics.
+ */
 #define TUE_SSE
 #endif
 
 #if defined(__SSE2__) \
     || (defined(_M_IX86_FP) && _M_IX86_FP >= 2) \
     || defined(_M_X64)
+/*!
+ * \brief Defined if the current compiler configuration supports SSE2
+ *        intrinsics.
+ */
 #define TUE_SSE2
 #endif
+
+/*!@}*/
